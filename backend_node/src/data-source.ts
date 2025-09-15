6be39f44cd824a3e947a8ed5949cc045afb362ec
ngkk00 <kidngkk@gmail.com>
2025-09-15 15:31:20 +0800

    Add postgre host

--- backend_node/src/data-source.ts
+++ backend_node/src/data-source.ts
@@ -15,7 +15,8 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',          // docker compose 
+  // host: process.env.DB_HOST || 'localhost',      // if npm run dev
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
