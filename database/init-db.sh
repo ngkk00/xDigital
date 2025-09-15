11679103c2d68a77349c17b80538f80404fc5cb2
ngkk00 <kidngkk@gmail.com>
2025-09-15 15:54:33 +0800

    Edit init script for db setup

--- database/init-db.sh
+++ database/init-db.sh
@@ -2,14 +2,12 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER postgres WITH PASSWORD 'mypassword';
     CREATE DATABASE xdigital;
-    GRANT ALL PRIVILEGES ON DATABASE xdigital TO postgres;
-    
+    GRANT ALL PRIVILEGES ON DATABASE xdigital TO $POSTGRES_USER;
+
     \c task_assignment_db;
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    
-    -- Grant permissions to postgres
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO postgres;
+
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
 EOSQL
